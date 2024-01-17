a9a75dfcd9547c4088b8262f2c7a3371ded1965b
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-01-17 01:50:23 +0100

    feat: add read_labels function which helps in loading labels from Handwritten Digit MNIST dataset

--- src/load_data.py
+++ src/load_data.py
@@ -1,6 +1,7 @@
 import gzip
 import torch
 from torch.nn.functional import one_hot
+from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import struct
@@ -8,19 +9,20 @@ import struct
 images_transform = ToTensor
 labels_transform = one_hot
 
-def visualize_sample(images, labels, rows, cols):
-    figure = plt.figure(figsize=(rows * 3, cols * 3))
+def visualize_sample(images: torch.Tensor, labels: torch.Tensor, rows: int, cols: int):
+    figure = plt.figure(figsize=(rows * 2, cols * 2))
     for i in range(1, rows * cols + 1):
         sample_idx = torch.randint(len(images), size=(1,)).item()
         img = images[sample_idx].squeeze()
-        label = labels[sample_idx]
+        label = labels[sample_idx].item()
+        img_title = f'Written digit : {label}'
         figure.add_subplot(rows, cols, i)
         plt.axis('off')
         plt.imshow(img, cmap=plt.cm.gray)
-        plt.title(label)
+        plt.title(img_title)
     plt.show()
 
-def read_images(path, image_size, num_items):
+def read_images(path: str, num_items: int | None = None):
     expected_magic = 2051
     metadata_size = 16
     with gzip.open(path, 'rb') as file:
@@ -30,29 +32,47 @@ def read_images(path, image_size, num_items):
             raise ValueError(f'Magic number mismatch, expected {expected_magic}, got {magic}')
         data = torch.frombuffer(file.read(), dtype=torch.uint8)
 
-    if size > num_items:
+    if num_items and size > num_items:
         size = num_items
 
     images = torch.zeros((size, rows, cols))
     for i in range(size):
         if i > size:
             break
-        img = data[i * rows * cols:(i + 1) * rows * cols] / 255.0
-        images[i] = images[i].put(
-            index=torch.tensor(range(rows * cols)),
-            source=img)
-        images[i] = images[i].reshape((rows, cols))
+        image = data[i * rows * cols:(i + 1) * rows * cols] / 255.0
+        images[i][:] = image.reshape(rows, cols)
     return images
 
+def read_labels(path: str, num_items: int | None = None):
+    expected_magic = 2049
+    metadata_size = 8
+    with gzip.open(path, 'rb') as file:
+        magic, size = struct.unpack('>II', file.read(metadata_size))
+        print(magic, size)
+        if magic != expected_magic:
+            raise ValueError(f'Magic number mismatch, expected {expected_magic}, got {magic}')
+
+        if num_items and size > num_items:
+            size = num_items
+
+        data = torch.frombuffer(file.read(size), dtype=torch.uint8)
+    return data
+
 def load_handwritten_mnist_data():
-    image_size = 28
     num_train = 60_000
     num_test = 10_000
-    training_images = read_images("data/train-images-idx3-ubyte.gz", image_size, num_train)
-    test_images = read_images("data/t10k-images-idx3-ubyte.gz", image_size, num_test)
+    training_images = read_images("data/train-images-idx3-ubyte.gz", num_train)
+    training_labels = read_labels("data/train-labels-idx1-ubyte.gz", num_train)
+    test_images = read_images("data/t10k-images-idx3-ubyte.gz", num_test)
+    test_labels = read_labels("data/t10k-labels-idx1-ubyte.gz", num_test)
     
-    return training_images, test_images
+    return (training_images, training_labels), (test_images, test_labels)
 
 if __name__ == "__main__":
-    t1, t2 = load_handwritten_mnist_data()
-    print(f"{len(t1) = }, {len(t1) = }")
+    (t1, l1), (t2, l2) = load_handwritten_mnist_data()
+
+    print(f"{len(t1) = }, {len(l1) = }")
+    print(f"{len(t2) = }, {len(l2) = }")
+
+    visualize_sample(t1, l1, 3, 3)
+
