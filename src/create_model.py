dc78d527c043eda3253306dd205e7172fadc87b4
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-01-18 17:39:52 +0100

    feat: load data and create model for classification

---
+++ src/create_model.py
@@ -0,0 +1,39 @@
+import torch
+from torchvision.transforms import Lambda
+from load_data import HandwrittenDigitMNIST
+from classifier import HandwrittenDigitClassifier
+
+if __name__ == "__main__":
+    # Initialize transforms
+    images_transform = Lambda(lambda image: image.reshape(28, 28).divide(255))
+    labels_transform = Lambda(lambda label: torch.zeros(10).put(label.type(torch.int64), torch.tensor(1.0)))
+
+    # Set training and test files' paths
+    training_images_filepath = "data/train-images-idx3-ubyte.gz"
+    training_labels_filepath = "data/train-labels-idx1-ubyte.gz"
+    test_images_filepath = "data/t10k-images-idx3-ubyte.gz"
+    test_labels_filepath = "data/t10k-labels-idx1-ubyte.gz"
+
+    # Create training dataset
+    training_data = HandwrittenDigitMNIST(
+        training_images_filepath,
+        training_labels_filepath,
+        images_transform,
+        labels_transform)
+    # Create test dataset
+    test_data = HandwrittenDigitMNIST(
+        test_images_filepath,
+        test_labels_filepath,
+        images_transform,
+        labels_transform)
+
+    # Create model
+    model = HandwrittenDigitClassifier(512, 512)
+    
+    # Compile model
+
+    # Fit model to training data
+
+    # Evaluate model
+
+    # Save model
