232edd55083d33251828757e5d15b2c63cc61901
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-02-18 18:19:44 +0100

    feat: add model fitting to training data

--- src/classifier.py
+++ src/classifier.py
@@ -1,15 +1,10 @@
 from torch import nn, Tensor
+import torch
 
 class HandwrittenDigitClassifier(nn.Module):
     def __init__(self):
         # Call super constructor
         super(HandwrittenDigitClassifier, self).__init__()        
-
-        # Constants for input and ouput layers's size
-        image_width = 28
-        image_height = 28
-        nb_classes = 10
-
         # Create layers
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 3)
@@ -26,5 +21,61 @@ class HandwrittenDigitClassifier(nn.Module):
         x = nn.functional.relu(self.conv3(x))
         x = self.flat(x)
         x = nn.functional.relu(self.fc1(x))
-        x = nn.functional.log_softmax(self.fc2(x), dim=1)
+        x = self.fc2(x)
         return x
+    
+    def fit(self, train_dataloader, test_dataloader, epochs=10, optimizer=None, loss_fn=None):
+        # Ensure training parameters initialization
+        lr = 0.01
+        optimizer = optimizer or torch.optim.SGD(self.parameters(), lr)
+        loss_fn = loss_fn or nn.NLLLoss()
+        # Create training results dictionary
+        res = {'Train accuracy': [], 'Validation accuracy': [], 'Train loss': [], 'Validation loss': []}
+
+        # Learning loop
+        for i in range(1, epochs + 1):
+            ta, tl = self.train_epoch(train_dataloader, optimizer, loss_fn)
+            va, vl = self.validate_epoch(test_dataloader, loss_fn)
+            print(f'Epoch {i:2}: Train acc={ta:.2f}%, Val acc={va:.2f}%, Train loss={tl:.4f}, Val loss={vl:.4f}')
+            res['Train accuracy'].append(ta)
+            res['Train loss'].append(tl)
+            res['Validation accuracy'].append(va)
+            res['Validation loss'].append(vl)
+        # Return training results            
+        return res
+    
+    def train_epoch(self, dataloader, optimizer, loss_fn):
+        acc, loss = 0, 0
+        data_size = len(dataloader.dataset)
+
+        for features, labels in dataloader:
+            labels = labels.view(len(labels))
+            # Prediction phase
+            out = self(features)
+            # Learning phase
+            optimizer.zero_grad()
+            batch_loss = loss_fn(out, labels)
+            batch_loss.backward()
+            optimizer.step()
+            # Update accuracy and loss
+            loss += batch_loss
+            pred = out.max(dim=1)[1]
+            acc += (pred == labels).type(torch.float).sum()
+        # Return epoch results
+        return (acc.item() / data_size) * 100, loss.item() / data_size
+    
+    def validate_epoch(self, dataloader, loss_fn):
+        acc, loss = 0, 0
+        data_size = len(dataloader.dataset)
+
+        with torch.no_grad():
+            for features, labels in dataloader:
+                labels = labels.view(len(labels))
+                # Prediction phase
+                out = self(features)
+                # Update loss and accuracy
+                loss += loss_fn(out, labels)
+                pred = out.max(dim=1)[1]
+                acc += (pred == labels).type(torch.float).sum()
+        # Return epoch results
+        return (acc.item() / data_size) * 100, loss.item() / data_size
--- src/create_model.py
+++ src/create_model.py
@@ -1,13 +1,15 @@
 import torch
 from torchinfo import summary
+from torch.utils.data import DataLoader
 from torchvision.transforms import Lambda
 from load_data import HandwrittenDigitMNIST
 from classifier import HandwrittenDigitClassifier
 
 if __name__ == "__main__":
+    model_path = 'model/hwd_classifier.pth'
     # Initialize transforms
-    images_transform = Lambda(lambda image: image.reshape(28, 28).divide(255))
-    labels_transform = Lambda(lambda label: torch.zeros(10).put(label.type(torch.int64), torch.tensor(1.0)))
+    images_transform = Lambda(lambda image: image.reshape(1, 28, 28).divide(255))
+    # labels_transform = Lambda(lambda label: torch.zeros(10).put(label.type(torch.int64), torch.tensor(1.0)))
 
     # Set training and test files' paths
     training_images_filepath = "data/train-images-idx3-ubyte.gz"
@@ -19,14 +21,17 @@ if __name__ == "__main__":
     training_data = HandwrittenDigitMNIST(
         training_images_filepath,
         training_labels_filepath,
-        images_transform,
-        labels_transform)
+        images_transform)
     # Create test dataset
     test_data = HandwrittenDigitMNIST(
         test_images_filepath,
         test_labels_filepath,
-        images_transform,
-        labels_transform)
+        images_transform)
+
+    # Create train and test dataloader
+    batch_size = 64
+    train_dataloader = DataLoader(training_data, batch_size)
+    test_dataloader = DataLoader(test_data, batch_size)
 
     # Create model
     model = HandwrittenDigitClassifier()
@@ -34,10 +39,17 @@ if __name__ == "__main__":
     # Model summary
     summary(model, input_size=(1, 1, 28, 28))
     
-    # Compile model
+    # Set training parameters
+    learning_rate = 0.01
+    epochs = 5
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), learning_rate)
 
     # Fit model to training data
+    res = model.fit(train_dataloader, test_dataloader, epochs, optimizer, loss_fn)
 
-    # Evaluate model
+    # Plot training results
 
     # Save model
+    # torch.save(model.parameters(), model_path)
+
