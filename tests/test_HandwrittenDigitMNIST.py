a0d7faefece0d57e5dc7b5bb587e5c6fc88c5188
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-02-20 20:39:47 +0100

    tests: add unitest for loading dataset and checking its size

---
+++ tests/test_HandwrittenDigitMNIST.py
@@ -0,0 +1,23 @@
+from sys import path
+
+src_path = 'src'
+path.insert(0, src_path)
+
+from load_data import HandwrittenDigitMNIST
+
+class TestHandwrittenDigitMNIST:
+    # Data files' paths
+    test_images_filepath = "data/t10k-images-idx3-ubyte.gz"
+    test_labels_filepath = "data/t10k-labels-idx1-ubyte.gz"
+
+    def test_dataset_size(self):
+        # Arrangement
+        expected_size = 10000
+
+        # Action
+        test_data = HandwrittenDigitMNIST(
+            self.test_images_filepath,
+            self.test_labels_filepath)
+
+        # Assertion
+        assert len(test_data) == expected_size
